1b66eefaa23daa39d229874e7175453e9e7b88b5
IlyaDischenko <ilyadischenko@gmail.com>
2023-07-31 16:04:26 +0300

    commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from 'react-router-dom'
 import { Footer } from '../footer/Footer'
 
 import s from './Main.module.scss'
+import { CatItem } from './catalogItem/CatItem'
 
 export const Main = () => {
     const navigate = useNavigate()
 
     return (
         <div className={s.wrapper} onClick={() => navigate("/")}>
-            sad
+            <div className={s.items}>
+                <CatItem />
+            </div>
 
             <Footer />
         </div>
--- src/components/main/catalogItem/CatItem.jsx
+++ src/components/main/catalogItem/CatItem.jsx
@@ -2,12 +2,25 @@
 
 import s from './CatItem.module.scss'
 
-export const Main = () => {
+import scoopy from './../../../static/images/bikes/scoopy.png'
+
+export const CatItem = () => {
 
     return (
-        <div className={s.wrapepr}>
+        <div className={s.wrapper}>
+            <div className={s.image}>
+                <img src={scoopy} alt="Bike" />
+            </div>
 
-
+            <div className={s.info}>
+                <div className={s.title_price}>
+                    <span>Мини скутеры</span>
+                    <span>от 3$</span>
+                </div>
+                <div className={s.subtitle}>
+                    <span>Для девушек / одиночных поездок</span>
+                </div>
+            </div>
         </div>
     )
 }
